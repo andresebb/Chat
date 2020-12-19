e73d6ae2bdca2fa686196176c4ca961225f5c768
Andres Betancourt <aebb001@gmail.com>
2020-12-18 20:43:56 -0400

    Creando usuario en base de datos

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,8 +1,33 @@
+const Usuario = require("../models/usuario");
+
 const crearUsuario = async (req, res) => {
-  res.json({
-    ok: true,
-    msg: "New User",
-  });
+  try {
+    const { email, password } = req.body;
+
+    //Verificar si existe el email
+    const existeEmail = await Usuario.findOne({ email });
+
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El email ya existe",
+      });
+    }
+
+    //Guardar usuario en base de datos
+    const newUser = new Usuario(req.body);
+    await newUser.save();
+
+    res.json({
+      newUser,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 const login = async (req, res) => {
