e193fb75624b045639a86be892bc7ff0746a2ea2
Andres Betancourt <aebb001@gmail.com>
2020-12-21 10:39:33 -0400

    Rutas Secundarias

--- chat-app/src/router/AppRouter.js
+++ chat-app/src/router/AppRouter.js
@@ -3,22 +3,20 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Route,
-  NavLink,
   Switch,
   Redirect,
 } from "react-router-dom";
 import { ChatPage } from "../pages/ChatPage";
-import { LoginPage } from "../pages/LoginPage";
-import { RegisterPage } from "../pages/RegisterPage";
+
+import { AuthRouter } from "./AuthRouter";
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/chat" component={ChatPage} />
+          <Route path="/auth" component={AuthRouter} />
+          <Route exac path="/" component={ChatPage} />
           <Redirect to="/" />
         </Switch>
       </div>
